acc12e31b01054958d47ea9e8f0f717a023b6872
kuno <ayana.kuno@mixi.co.jp>
2021-05-03 17:06:29 +0900

    罰画像の追加とボタン押せなくなるように

---
+++ covid-game/avoid-yuriko/gameData.swift
@@ -0,0 +1,13 @@
+//
+//  gameData.swift
+//  covid-game
+//
+//  Created by ayana.kuno on 2021/05/03.
+//
+
+import Foundation
+
+struct GameData: Codable {
+    var point: Int = 0
+    var flag: Bool = false
+}
--- covid-game/avoid-yuriko/gameView.swift
+++ covid-game/avoid-yuriko/gameView.swift
@@ -10,58 +10,75 @@ import SwiftUI
 struct GameView: View {
     @ObservedObject var viewModel = GameViewModel()
     
-    @State var point: Int = 0
+    @State var gameData = GameData()
+    
     // 今日の日付（Date()）にDateComponents(second: 31)を追加
     let time = Calendar.current.date(byAdding: DateComponents(second: 31), to: Date())!
     var body: some View {
-        VStack {
-            HStack(alignment: .center) {
-                Image(viewModel.getYuriko())
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 150, height: 150, alignment: .center)
+        ZStack(alignment: .center) {
+            VStack {
+                HStack(alignment: .center) {
+                    Image(viewModel.getYuriko())
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150, alignment: .center)
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image("money")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50, alignment: .center)
+                            Text(String(gameData.point))
+                        }
+                        HStack {
+                            Image("timer")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50, alignment: .center)
+                            // style: .timerでカウントダウン表示に
+                            Text(time, style: .timer)
+                            // TODO: timeが0になったら終わりの表示
+                        }
+                    }
+                    .padding(.trailing, 50)
+                }
+                .padding(.top, 50)
                 Spacer()
-                VStack(alignment: .leading) {
-                    HStack {
-                        Image("money")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50, alignment: .center)
-                        Text(String(point))
+                HStack(alignment: .center) {
+                    Button(action: {
+                        if viewModel.getYuriko() == "yuriko-back" {
+                            gameData.point += 200
+                        } else {
+                            gameData.flag = true
+                        }
+                    }) {
+                    Image("alcohol")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .center)
                     }
-                    HStack {
-                        Image("timer")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50, alignment: .center)
-                        // style: .timerでカウントダウン表示に
-                        Text(time, style: .timer)
-                        // TODO: timeが0になったら終わりの表示
+                    // 罰の間はボタンを押せなくする
+                    .disabled(gameData.flag)
+                    Button(action: {
+                        gameData.point += 100
+                    }) {
+                    Image("non-alcohol")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .center)
                     }
+                    .disabled(gameData.flag)
                 }
-                .padding(.trailing, 50)
+                .padding(.bottom, 100)
             }
-            .padding(.top, 50)
-            Spacer()
-            HStack(alignment: .center) {
-                Button(action: {
-                    point += 200
-                }) {
-                Image("alcohol")
+            if gameData.flag {
+                Image("batsu")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 100, height: 100, alignment: .center)
-                }
-                Button(action: {
-                    point += 100
-                }) {
-                Image("non-alcohol")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100, alignment: .center)
-                }
+                    .frame(width: 300, height: 300, alignment: .center)
+                    .padding(.top, 100)
             }
-            .padding(.bottom, 100)
         }
     }
 }
