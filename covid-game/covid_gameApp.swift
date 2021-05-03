190e216b9c0ca577d31864bc400f8eaf6de6c95a
kuno <ayana.kuno@mixi.co.jp>
2021-05-03 11:02:24 +0900

    Initial Commit

---
+++ covid-game/covid_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  covid_gameApp.swift
+//  covid-game
+//
+//  Created by ayana.kuno on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct covid_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
