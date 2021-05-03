b0820ff052315e6d7fcd73cfb14aabaec6098257
kuno <ayana.kuno@mixi.co.jp>
2021-05-03 14:03:15 +0900

    名前の変更とゆりこが振り向くように

--- covid-game/avoid-yuriko/gameView.swift
+++ covid-game/avoid-yuriko/gameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  GameView.swift
 //  covid-game
 //
 //  Created by ayana.kuno on 2021/05/03.
@@ -7,13 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GameView: View {
+    @ObservedObject var viewModel = GameViewModel()
+    
     @State var point: Int = 0
-    let time = Calendar.current.date(byAdding: DateComponents(second: 61), to: Date())!
+    @State var yuriko: String = "yuriko-back"
+    // 今日の日付（Date()）にDateComponents(second: 31)を追加
+    let time = Calendar.current.date(byAdding: DateComponents(second: 31), to: Date())!
     var body: some View {
         VStack {
             HStack(alignment: .center) {
-                Image("yuriko-back")
+                Image(getYuriko())
                     .resizable()
                     .scaledToFit()
                     .frame(width: 150, height: 150, alignment: .center)
@@ -31,6 +35,7 @@ struct ContentView: View {
                             .resizable()
                             .scaledToFit()
                             .frame(width: 50, height: 50, alignment: .center)
+                        // style: .timerでカウントダウン表示に
                         Text(time, style: .timer)
                         // TODO: timeが0になったら終わりの表示
                     }
@@ -60,10 +65,22 @@ struct ContentView: View {
             .padding(.bottom, 100)
         }
     }
+    
+    func getYuriko() -> String {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            let turn = Bool.random()
+            if turn {
+                yuriko = "yuriko-front"
+            } else {
+                yuriko = "yuriko-back"
+            }
+        }
+        return yuriko
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        GameView()
     }
 }
--- covid-game/covid_gameApp.swift
+++ covid-game/covid_gameApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct covid_gameApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GameView()
         }
     }
 }
