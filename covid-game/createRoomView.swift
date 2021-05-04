3014e5cd9d0ab5840d746e7a67e6be77b1eee0f2
kuno <ayana.kuno@mixi.co.jp>
2021-05-04 20:46:10 +0900

    ルーム作成の挙動追加

---
+++ covid-game/createRoomView.swift
@@ -0,0 +1,66 @@
+//
+//  createRoomView.swift
+//  covid-game
+//
+//  Created by ayana.kuno on 2021/05/04.
+//
+
+import SwiftUI
+
+struct createRoomView: View {
+    @State private var showCopyAlert = false
+    @State private var startFlag = false
+
+    var body: some View {
+        ZStack {
+            Image("background-create-room")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 400, height: 440, alignment: .center)
+            VStack {
+                ZStack {
+                    // バックエンドからはroom_idとuser_id送られてくる
+                    Image("room-id")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 350, height: 200, alignment: .center)
+                    Text("ababa")
+                        .frame(width: 200, height: 100, alignment: .bottom)
+                }
+                Button(action: {
+                    self.showCopyAlert = true
+                    UIPasteboard.general.string = "ababa"
+                }) {
+                Image("copy")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 80, alignment: .center)
+                }
+                .alert(isPresented: $showCopyAlert) {
+                    Alert(title: Text("コピーしました！"))
+                }
+                if startFlag {
+                    Button(action: {
+                        // avoid-yurikoのゲーム画面に遷移
+                    }) {
+                    Image("gamestart-active")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 80, alignment: .center)
+                    }} else {
+                    // 人数揃うまでボタン押せない
+                    Image("gamestart-non-active")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 80, alignment: .center)
+                }
+            }
+        }
+    }
+}
+
+struct createRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        createRoomView()
+    }
+}
---
+++ covid-game/selectRoomView.swift
@@ -0,0 +1,44 @@
+//
+//  selectRoomView.swift
+//  covid-game
+//
+//  Created by ayana.kuno on 2021/05/04.
+//
+
+import SwiftUI
+
+struct selectRoomView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Image("background-select-room")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 350, height: 350, alignment: .center)
+                VStack {
+                    // 画面遷移
+                    NavigationLink(destination: createRoomView()) {
+                        Image("create-room")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 100, alignment: .center)
+                    }
+                    Button(action: {
+                     
+                    }) {
+                    Image("join-room")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 100, alignment: .center)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct selectRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        selectRoomView()
+    }
+}
